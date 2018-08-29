5c4ee69b2047284dea5b756258d2a91a8eaee0cd
23 <3123@q.c>
2018-08-29 14:26:33 +0800

    精简程序

--- sycmDanpin.user.js
+++ sycmDanpin.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         sycm+danpin : sycm danpin
+// @name         sycm danpin
 // @name:zh      单品分析助手-生意参谋 chazz
 // @namespace    https://github.com/chazz1/taobaosycm
 // @version      0.1
@@ -17,35 +17,31 @@
     $(".btn-group-switch .ui-switch-menu").append(btn1);
 
     $("li.ch-btn").click(function(){
-        $("span.ch-span").remove() //先清空计算标签
-
-
+        $("span.ch-span").remove() 
         var sourceNames = $("li.t-source-name ul.flow-source-table").children("ul").children("li");
         var flows = $("li.t-flow ul.flow-source-table").children("ul").children("li");
         var guides = $("li.t-guide ul.flow-source-table").children("ul").children("li");
         sourceNames.each(function(){
-            // var sourceName = $(this).text(); //获取渠道名称
-            var i = $(this).index(); //获取当前行数，从0开始计算
-            var fangke = flows.eq(i).children("div").eq(0); //获取访客对象
-            var shoucang = guides.eq(i).children("div").eq(0); //获取收藏人数对象
-            var jiagou = guides.eq(i).children("div").eq(1); //获取加购人数对象
-            var fk = fangke.text(); //获取访客数据
-            var jg = jiagou.text();//获取加购数据
-            var sc = shoucang.text()//获取收藏人数
-
-            var reg = new RegExp(",",""); //去掉数字中的,号，以防计算出现NaN的情况
-            fk = Number(fk.replace(reg,"")); //整理数据
+            var i = $(this).index(); 
+            var fangke = flows.eq(i).children("div").eq(0); 
+            var shoucang = guides.eq(i).children("div").eq(0); 
+            var jiagou = guides.eq(i).children("div").eq(1); 
+            var fk = fangke.text(); 
+            var jg = jiagou.text();
+            var sc = shoucang.text();
+            var reg = new RegExp(",",""); 
+            fk = Number(fk.replace(reg,"")); 
             jg = Number(jg.replace(reg,""));
             sc = Number(sc.replace(reg,""));
-            var js = (jg/fk)*100;  //计算加购率
+            var js = (jg/fk)*100;  
             var t = '<span class="ch-span" style="color: #f00;font-size:6px;">'+js.toFixed(1)+'%</span>';
-            jiagou.append(t); //插入加购率数据
-            js = ((sc+jg)/fk)*100;  //计算加购率
+            jiagou.append(t); 
+            js = ((sc+jg)/fk)*100;  
             t = '<span class="ch-span" style="color: #f00;font-size:6px;">'+js.toFixed(1)+'%</span>';
-            fangke.append(t); //插入加购收藏率数据
-            js = (sc/fk)*100;  //计算收藏率
+            fangke.append(t); 
+            js = (sc/fk)*100;  
             t = '<span class="ch-span" style="color: #f00;font-size:6px;">'+js.toFixed(1)+'%</span>';
-            shoucang.append(t); //插入收藏率数据
+            shoucang.append(t); 
         });
     });
 
--- sycmXiaoguo.user.js
+++ sycmXiaoguo.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         sycm+xiaoguo : sycm xiaoguo
+// @name         sycm xiaoguo
 // @name:zh      商品效果助手-生意参谋 chazz
 // @namespace    https://github.com/chazz1/taobaosycm
 // @version      0.1
@@ -15,68 +15,59 @@
     'use strict';
     var btn1 = '<div class="ui-switch btn-group-switch"><ul class="ui-switch-menu"><li class="ui-switch-item ui-routable-item ch-btn hidden"><a href="JavaScript:;">计算收藏加购率</a></li><li class="ui-switch-item ui-routable-item ch-btn2"><a href="JavaScript:;">看人气</a></li><li class="ui-switch-item ui-routable-item ch-btn3"><a href="JavaScript:;">看价值</a></li></ul></div>';
     $(".operation").prepend(btn1);
-
     $("li.ch-btn2").click(function(){
-        if($(".ch-btn").hasClass("hidden")){//如果没有计算按钮
-            $(".ch-btn").removeClass("hidden");//显示计算按钮
+        if($(".ch-btn").hasClass("hidden")){
+            $(".ch-btn").removeClass("hidden");
         }
         gouxuan(4,5,20,21);
     });
     $("li.ch-btn3").click(function(){
-        if(!$(".ch-btn").hasClass("hidden")){//如果有计算按钮
-            $(".ch-btn").addClass("hidden");//隐藏计算按钮
+        if(!$(".ch-btn").hasClass("hidden")){
+            $(".ch-btn").addClass("hidden");
         }
         gouxuan(3,10,14,15);
-
     });
-
     $("li.ch-btn").click(function(){
-        $("span.ch-span").remove() //先清空计算标签
-
+        $("span.ch-span").remove() 
         var div = $("div.orderable-table");
         var divHeader= div.children("div.table-header");
         var divBody= div.children("div.table-body");
         var rows = divBody.children("div.row");
         rows.each(function(){
             var spans = $(this).children("span");
-            var fangke = spans.eq(1); //获取访客对象
-            var jiagou = spans.eq(2); //获取加购对象
-            var shoucang = spans.eq(3); //获取收藏对象
-            var fk = fangke.text();//获取访客数值
+            var fangke = spans.eq(1); 
+            var jiagou = spans.eq(2); 
+            var shoucang = spans.eq(3);
+            var fk = fangke.text();
             var jg = jiagou.text();
             var sc = shoucang.text();
-            var reg = new RegExp(",",""); //去掉数字中的,号，以防计算出现NaN的情况
-            fk = Number(fk.replace(reg,"")); //整理数据
+            var reg = new RegExp(",",""); 
+            fk = Number(fk.replace(reg,"")); 
             jg = Number(jg.replace(reg,""));
             sc = Number(sc.replace(reg,""));
-            var js = (jg/fk)*100;  //计算加购率
+            var js = (jg/fk)*100;  
             var t = '<span class="ch-span" style="color: #f00;font-size:6px;"><br>'+js.toFixed(1)+'%</span>';
-            jiagou.append(t); //插入加购率数据
-            js = ((sc+jg)/fk)*100;  //计算加购率
+            jiagou.append(t); 
+            js = ((sc+jg)/fk)*100;  
             t = '<span class="ch-span" style="color: #f00;font-size:6px;"><br>'+js.toFixed(1)+'%</span>';
-            fangke.append(t); //插入加购收藏率数据
-            js = (sc/fk)*100;  //计算收藏率
+            fangke.append(t); 
+            js = (sc/fk)*100;  
             t = '<span class="ch-span" style="color: #f00;font-size:6px;"><br>'+js.toFixed(1)+'%</span>';
-            shoucang.append(t); //插入收藏率数据
-
+            shoucang.append(t); 
         });
-
     });
-
 })();
 function gouxuan(s1,s2,s3,s4){
-
-    $("span.ch-span").remove() //先清空计算标签
-
+    $("span.ch-span").remove() 
     var dropdown=$("div.dropdown-index-picker");
     var spans = dropdown.find("span.checkbox");
-    spans.each(function(i,item){//先清空除了第一个以外其他所有复选框
+    spans.each(function(i,item){
         var checkbox=$(this);
         if(i!=0){
             if(checkbox.hasClass("selected")){checkbox.click();}
         }
     });
-    spans.each(function(i,item){//选择根据需要的复选框
+    spans.each(function(i,item){
         var checkbox=$(this);
         switch(i){
             case s1:
